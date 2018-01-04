7712135379e07cee14b8ff1e06ceb372d78bdc3d
Stefan Becker <ich@funbaker.de>
2018-01-05 00:22:23 +0100

    fill meta file

---
+++ donation_de/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Stefan Becker <s.becker@humanilog.org>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Donation DE",
+    "summary": "Everything you need for managing donations in Germany",
+    "version": "9.0.1.0.0",
+    "category": "Donation Management",
+    'website': 'http://www.humanilog.org',
+    'author': 'Humanilog, '
+              'Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "donation",
+    ],
+    "data": [
+        'views/report.xml'
+    ],
+}
